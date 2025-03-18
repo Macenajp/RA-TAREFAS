2c149cf25df3b761a41cc8c3af38198dd561bfff
João Pedro Macena <joaomacenactba@gmail.com>
2025-03-18 18:25:09 -0300

    Create pessoaFísica.py

---
+++ pessoaFísica.py
@@ -0,0 +1,6 @@
+nome = input('Qual o seu nome?')
+cpf = input('Qual o seu CPF?')
+celular = input('Qual seu número?')
+nascimento = input('Qual sua data de nascimento')
+
+print('Suas informações são:' , nome, cpf, celular, nascimento)

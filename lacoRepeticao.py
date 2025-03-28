e4c5c60950dbe7556fd04449105c68ce52380705
João Pedro Macena <joaomacenactba@gmail.com>
2025-03-28 17:21:57 -0300

    Add files via upload

---
+++ lacoRepeticao.py
@@ -0,0 +1,9 @@
+# escrever um programa Python que
+# imprime os números de 1 a 10
+
+contador = 1
+while contador <= 10:
+    print('numero: ', contador)
+    contador = contador + 1 # contador += 1
+
+print('fim')
